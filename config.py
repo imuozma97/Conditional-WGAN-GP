37f9131a64c6e5c42658dad74ffb6db0d2624ce1
Maria Iranzo Muñoz <maimu2@alumni.uv.es>
2026-04-22 15:50:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,26 @@
 """
-Este archivo contiene als constantes necesarias durante el entrenamiento
+Parámetros que se mantienen constantes durante el entrenamiento.
+
 """
-batch_size1 = 17
-batch_size2 = 34
-ncritic1 = 3
-ncritic2 = 2
-ncritic3 = 1
 
 num_classes = 34 
 image_size = 64
 latent_dim = 128
 num_cv = 27
 mass = 6.5e7 #Msol/h
-boxsize = 25
+boxsize = 25 #Poner unidades
 n_bar = 256**3 / 64**3
-N = 27
 embedding_dim = 32
 buffer_size = 918
 
+batch_size1 = 17
+batch_size2 = 34
+
+ncritic1 = 1
+ncritic2 = 2
+ncritic3 = 3
+
+N = 27
 
 shift = 1
 c = 20000
-#maximo_desnorm = 2.380412
-#minimo_desnorm = -0.20996958
