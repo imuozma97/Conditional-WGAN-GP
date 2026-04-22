add459fd733fc837decb10199a88dc927c5d9c84
Maria Iranzo Muñoz <maimu2@alumni.uv.es>
2026-04-22 15:54:32 +0200

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -1,22 +1,25 @@
 """
-Este archivo contiene las funciones de forward y backward
+Funciones forward y backward de momento. Mirar si aquó podría añdir más funciones que hagan alguna transformación
 """
-
-from functools import partial
 import tensorflow as tf
-import numpy as np
+from functools import partial
+
 from config import shift, c
 
-def stat_forward_0(x, c=2e4):
+
+
+def stat_forward_0_tf(x, c=2e4):
     
-    if not type(x).__module__ == np.__name__:
-        x = np.array([x])
-    res = np.zeros(shape=x.shape, dtype=np.float32)
+    x = tf.convert_to_tensor(x, dtype=tf.float32)
+
     mask = x > c
-    maski = mask == False
-    res[maski] = np.log(x[maski] + 1)
-    res[mask] = np.log(c + 1) + (x[mask] / c - 1)
-    res *= 3 / np.log(c + 1)
+    maski = tf.logical_not(mask)
+
+    res = tf.zeros_like(x, dtype=tf.float32)
+    res = tf.where(maski, tf.math.log(x + 1), res)
+    res = tf.where(mask, tf.math.log(c + 1) + (x / c - 1), res)
+
+    res = res * (3 / tf.math.log(c + 1))
     return res
 
 
@@ -30,15 +33,13 @@ def stat_backward_0(x, c=2e4):
 
     mask = x > mc
 
-    # rama 1: exp(x) - 1
     res_low = tf.math.exp(x) - 1.0
-    # rama 2: c * (x - log(c+1) + 1)
     res_high = c * (x - mc + 1.0)
-
-    # combinación
+    
     res = tf.where(mask, res_high, res_low)
-
     return res
+
+
 
 def stat_forward(x, c=2e4, shift=1):
     return stat_forward_0(x + shift, c=c) - stat_forward_0(shift, c=c)
