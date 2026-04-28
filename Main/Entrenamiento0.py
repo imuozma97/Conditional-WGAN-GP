f7d297ca07775acbb4070261a3e3fb1b467cd216
imuozma97 <maimu2@alumni.uv.es>
2026-04-28 12:31:40 +0200

    quito mixed-precision

--- Main/Entrenamiento0.py
+++ Main/Entrenamiento0.py
@@ -20,8 +20,8 @@ os.environ['TF_XLA_ENABLE'] = '0'
 
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras import mixed_precision
-mixed_precision.set_global_policy('mixed_float16')
+#from tensorflow.keras import mixed_precision
+#mixed_precision.set_global_policy('mixed_float16')
 # Optimizaciones de memoria
 tf.config.optimizer.set_jit(True)
 tf.config.optimizer.set_experimental_options({"layout_optimizer": False, "constant_folding": True, "shape_optimization": True, "arithmetic_optimization": True, "disable_meta_optimizer": False, "function_optimization": True})
