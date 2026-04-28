736ff7e5fdf0233cc2c46e3dc815e833b7cdff78
imuozma97 <maimu2@alumni.uv.es>
2026-04-28 10:18:37 +0200

    cambio en grad_pen por psd

--- grad_pen.py
+++ grad_pen.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 
-def gradient_penalty(real_images, fake_images, z_values, discriminator, batch_size, lambda_term ,extra_real = None):
+def gradient_penalty(real_images, fake_images, z_values, discriminator, batch_size, lambda_term , extra_real = None):
 
     alpha = tf.random.uniform([batch_size, 1, 1, 1, 1], 0., 1.)
     real_images = tf.cast(real_images, tf.float32)
@@ -15,7 +15,10 @@ def gradient_penalty(real_images, fake_images, z_values, discriminator, batch_si
     real_inputs = [interpolated, z_values]
 
     if extra_real is not None:
-        real_inputs.append(extra_real)
+        # Crear un PSD "dummy" para el gradient penalty sin calcular gradientes para él
+        # Usamos tf.stop_gradient para evitar que se calculen gradientes para el PSD
+        extra_real_stopped = tf.stop_gradient(extra_real)
+        real_inputs.append(extra_real_stopped)
 
 
     with tf.GradientTape() as gp_tape:
