0da2f2cd78750e26beee9d22bb688bfc4747f1a9
María <maimu2@alumni.uv.es>
2026-05-18 11:26:55 +0200

    añado varios lambda_schedule

--- psd_utils.py
+++ psd_utils.py
@@ -15,7 +15,7 @@ def psd_loss(gen_psd, mean_psd, sigma_log):
         
     return loss
 
-
+"""
 def lambda_psd_schedule(epoch):  #De este hacer revisión cuando lo vaya a usar
         
     if epoch < 150:
@@ -31,6 +31,51 @@ def lambda_psd_schedule(epoch):  #De este hacer revisión cuando lo vaya a usar
         lambda_psd = 0.15
         
     return lambda_psd
+"""
+
+def lambda_psd_schedule(epoch):
+
+    if epoch < 150:
+        lambda_psd = 0.0
+
+    elif epoch < 400:
+        lambda_psd = 0.1 + (0.5 - 0.1) * (epoch - 150) / (400 - 150)
+
+    elif epoch < 800:
+        lambda_psd = 0.5 + (1.0 - 0.5) * (epoch - 400) / (800 - 400)
+
+    elif epoch < 1200:
+        # se mantiene en 1.0
+        lambda_psd = 1.0
+
+    else:
+        lambda_psd = 1.2
+
+    return lambda_psd
+
+
+
+
+def lambda_psd_schedule2(epoch):
+
+    if epoch < 150:
+        lambda_psd = 0.0
+
+    elif epoch < 400:
+        lambda_psd = 0.5 + (1 - 0.5) * (epoch - 150) / (400 - 150)
+
+    elif epoch < 800:
+        lambda_psd = 1 + (1.3 - 1) * (epoch - 400) / (800 - 400)
+
+    elif epoch < 1200:
+        # se mantiene en 1.0
+        lambda_psd = 1.3
+
+    else:
+        lambda_psd = 1.5
+
+    return lambda_psd
+
 
 def psd_out_of_band_fraction(psd_gen, psd_min, psd_max):
     """
