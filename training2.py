984617fc57e240ccf839a4dbc0606de33192cf63
María <maimu2@alumni.uv.es>
2026-05-18 11:28:17 +0200

    para entrenar D con dos ramas

---
+++ training2.py
@@ -0,0 +1,336 @@
+"""
+Funciones principales del entrenamiento
+-train step: hace cada batch
+"""
+
+import tensorflow as tf
+import os
+import json
+import numpy as np
+
+
+from config import latent_dim
+from grad_pen import gradient_penalty
+from power import Power
+from psd_utils import psd_out_of_band_fraction, psd_loss
+from loss_plot import plot_loss_graph
+
+
+class Training(tf.keras.Model):
+
+    def __init__(self, data_class, discriminator1, discriminator2, generator, batch_size, ncritic, trained_models_folder, generated_images_folder, lambda_psd_schedule, use_psd=True, use_psd_loss = True):
+        super().__init__()
+        self.discriminator1 = discriminator1
+        self.discriminator2 = discriminator2
+        self.generator = generator
+        self.batch_size = batch_size
+        self.trained_models_folder= trained_models_folder
+        self.generated_images_folder = generated_images_folder
+        self.current_epoch = 0
+        self.ncritic = ncritic
+        self.use_psd = use_psd  #Aqui este use_psd se refiere a si uso la rama espectral del D
+        self.use_psd_loss = use_psd_loss
+        self.data_class = data_class
+        self.lambda_psd_schedule = lambda_psd_schedule
+
+        self.power = Power()
+
+
+    def compile(self, d_optimizer1, g_optimizer, d_optimizer2):
+        super().compile()
+        self.d_optimizer1 = d_optimizer1
+        self.g_optimizer = g_optimizer
+        self.d_optimizer2 = d_optimizer2
+
+
+    @tf.function    
+    def train_step(self, data):
+            
+        real_images, z_values, psd_max, psd_min, sigma_log, psd_real = data  
+
+        for _ in range(self.ncritic):
+            noise = tf.random.normal([self.batch_size, latent_dim]) 
+                
+            with tf.GradientTape(persistent=True) as disc_tape:
+                generated_images = self.generator([noise, z_values], training=True)
+                psd_gen = self.power.compute_all_psd(generated_images)
+
+                #Si el D tiene psd o no:
+                if self.use_psd:
+                    fake_predictions = self.discriminator1([generated_images, z_values], training=True)
+                    real_predictions = self.discriminator1([real_images, z_values], training=True)
+
+                    fake_predictions_spect = self.discriminator2([psd_gen, z_values], training=True)
+                    real_predictions_spect = self.discriminator2([psd_real, z_values], training=True)
+
+                else:
+                    fake_predictions = self.discriminator1([generated_images, z_values], training=True)
+                    real_predictions = self.discriminator1([real_images, z_values], training=True)
+                
+                gp, grads_norm_mean = gradient_penalty(real_images, generated_images, z_values, self.discriminator1, self.batch_size, 10)
+                    
+                disc_loss_fake = tf.reduce_mean(fake_predictions)
+                disc_loss_real = tf.reduce_mean(real_predictions)
+                wass_loss = disc_loss_fake - disc_loss_real
+
+                disc_loss = wass_loss + gp
+
+
+                disc_loss_spect_fake = tf.reduce_mean(fake_predictions_spect)
+                disc_loss_spect_real = tf.reduce_mean(real_predictions_spect)
+                wass_loss_spect = disc_loss_spect_fake - disc_loss_spect_real
+
+            grads_disc = disc_tape.gradient(disc_loss, self.discriminator1.trainable_variables)
+            norm_disc = tf.linalg.global_norm(grads_disc)
+            self.d_optimizer1.apply_gradients(zip(grads_disc, self.discriminator1.trainable_variables))
+
+            grads_disc_spect = disc_tape.gradient(wass_loss_spect, self.discriminator2.trainable_variables)
+            norm_disc_spect = tf.linalg.global_norm(grads_disc_spect)
+            self.d_optimizer2.apply_gradients(zip(grads_disc_spect, self.discriminator2.trainable_variables))
+
+            # Generador
+        noise = tf.random.normal([self.batch_size, latent_dim]) 
+        with tf.GradientTape() as gen_tape:
+                
+            generated_images = self.generator([noise, z_values], training=True)
+            psd_gen = self.power.compute_all_psd(generated_images)
+
+            #Si el D tiene psd o no:
+            if self.use_psd:
+                fake_predictions = self.discriminator1([generated_images, z_values], training=True)
+                fake_predictions_spect = self.discriminator2([psd_gen, z_values], training=True)
+            else:
+                fake_predictions = self.discriminator1([generated_images, z_values], training=True)
+                    
+            loss_adv = -tf.reduce_mean(fake_predictions)
+            loss_spect = -tf.reduce_mean(fake_predictions_spect)
+            gen_loss = loss_adv + 0.1*loss_spect
+
+            percent = psd_out_of_band_fraction(psd_gen, psd_min, psd_max)
+
+            loss_psd = psd_loss(psd_gen, psd_real, sigma_log) 
+
+            if self.use_psd_loss:
+                lambda_psd = self.lambda_psd_schedule(self.current_epoch)
+                gen_loss = loss_adv + lambda_psd*loss_psd
+            else:  
+                gen_loss = loss_adv
+                
+
+        grads_gen = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+        norm_gen = tf.linalg.global_norm(grads_gen)
+        self.g_optimizer.apply_gradients(zip(grads_gen, self.generator.trainable_variables))
+            
+        ratio1 = norm_disc / (norm_gen + 1e-8)
+
+        return wass_loss, disc_loss_real, disc_loss_fake, loss_adv, loss_psd, percent, grads_norm_mean, ratio1, psd_gen, psd_max, psd_min
+        
+    
+
+
+
+    def train(self, dataset_train,  epochs):
+
+        # Configuración de checkpoints
+        checkpoint_dir = os.path.join(self.trained_models_folder, "checkpoints")
+        checkpoint = tf.train.Checkpoint(generator = self.generator, discriminator = self.discriminator1, g_optimizer = self.g_optimizer, d_optimizer = self.d_optimizer, epoch = tf.Variable(0))
+        checkpoint_manager = tf.train.CheckpointManager(checkpoint, directory = checkpoint_dir, max_to_keep = 5)    
+        
+        loss_file = os.path.join(self.trained_models_folder, "loss_data.json")
+
+        # Restaurar si existe un checkpoint previo
+        if checkpoint_manager.latest_checkpoint:
+            print(f"Restaurando desde {checkpoint_manager.latest_checkpoint}")
+            checkpoint.restore(checkpoint_manager.latest_checkpoint)
+            start_epoch = int(checkpoint.epoch.numpy())  # Recuperar la última época guardada
+
+            if os.path.exists(loss_file):
+                print("Cargando histórico de pérdidas...")
+                with open(loss_file, 'r') as f:
+                    data = json.load(f)
+
+                epoch_vect = data.get("epoch_vect", [])
+                wass_losses = data.get('wass_losses', [])
+                disc_losses_f = data.get('disc_losses_f', [])
+                disc_losses_r = data.get('disc_losses_r', [])
+                adv_losses = data.get('adv_losses', [])
+                psd_losses = data.get('psd_losses', [])
+                grad_pen = data.get('grad_pen', [])
+                percents = data.get('percents', [])
+                ratios1 = data.get('ratio1', [])
+
+                best_epoch = data.get('best_epoch', [])
+                best_psd = data.get('best_psd', [])
+                best_percent = data.get('best_percent', [])
+                best_epoch_psd = data.get('best_epoch_psd', [])
+                best_epoch_percent = data.get('best_epoch_percent', [])
+
+                best_percent_metric = best_percent[-1]
+                best_psd_metric = best_psd[-1]
+
+
+
+        else:
+            print("No se encontraron checkpoints previos, iniciando desde cero.")
+            start_epoch = 0
+        
+            epoch_vect = []
+            wass_losses = []
+            disc_losses_r, disc_losses_f  = [],  []
+                
+            adv_losses = []
+            psd_losses = []
+                
+            grad_pen = []
+            percents = []
+                
+            ratios1 = []
+
+            best_percent_metric = float("inf")
+            best_psd_metric = float("inf")
+            best_psd, best_epoch_psd, best_percent, best_epoch_percent = [], [], [], []
+        
+            
+        for epoch in range(start_epoch, epochs):
+                
+            self.current_epoch = epoch
+            batch_count = 0
+            wass_loss, disc_loss_r, disc_loss_f  = 0, 0, 0
+            adv_loss = 0
+            psd_loss = 0
+            gp = 0
+            percent = 0
+            ratio1 = 0
+        
+            
+            print('Currently training on epoch {} (out of {}).'.format(epoch, epochs))
+
+            for image_batch in dataset_train:
+                losses = self.train_step(image_batch)
+                wass_loss += -losses[0]
+                disc_loss_r += losses[1]
+                disc_loss_f += losses[2]
+
+                adv_loss += losses[3]
+                psd_loss += losses[4]
+                percent += losses[5]
+                    
+                gp += losses[6]
+                    
+                ratio1 += losses[7]
+                
+                psd_gen_batch = losses[8]
+                psd_max_batch = losses[9]
+                psd_min_batch = losses[10]
+                percent_batch = losses[5]
+                    
+                batch_count += 1
+                    
+
+            wass_loss /= batch_count
+            disc_loss_r /= batch_count
+            disc_loss_f /= batch_count
+                
+            adv_loss /= batch_count
+            psd_loss /= batch_count
+            percent /= batch_count
+
+            gp /= batch_count
+                
+            ratio1 /= batch_count
+                
+
+            
+            #Aquí guardará el modelo únicamente si mejora
+            if epoch > 150 and percent < best_percent_metric:
+                best_percent_metric = percent
+
+                gen_path = os.path.join(self.trained_models_folder, "best_percent_generator", f"epoch_{epoch:05d}")
+                os.makedirs(gen_path, exist_ok=True)
+                self.generator.save(gen_path)
+
+                best_percent.append(float(percent.numpy()))
+                best_epoch_percent.append(epoch)
+
+                np.savez(os.path.join(gen_path, f"psd_data_{epoch:05d}.npz"),
+                    psd_gen = psd_gen_batch.numpy(),
+                    psd_min=psd_min_batch.numpy(),
+                    psd_max=psd_max_batch.numpy(),
+                    percent = float(percent_batch.numpy())
+                    )
+
+                print(f"Best percent guardado en época {epoch}")
+
+
+            if epoch > 150 and psd_loss < best_psd_metric:
+                best_psd_metric = psd_loss
+
+                gen_path = os.path.join(self.trained_models_folder, "best_psd_generator", f"epoch_{epoch:05d}")
+                os.makedirs(gen_path, exist_ok=True)
+                self.generator.save(gen_path)
+
+                best_psd.append(float(psd_loss.numpy()))
+                best_epoch_psd.append(epoch)
+
+                np.savez(os.path.join(gen_path, f"psd_data_{epoch:05d}.npz"),
+                    psd_gen = psd_gen_batch.numpy(),
+                    psd_min = psd_min_batch.numpy(),
+                    psd_max = psd_max_batch.numpy(),
+                    percent = float(percent_batch.numpy())
+                )
+
+                print(f"Best psd Guardado en época {epoch}")
+
+            
+                    
+                    
+        
+            if epoch % 150 == 0:
+                print(f"Guardando modelo por estabilización después de 150 épocas.")
+
+                gen_path = os.path.join(self.trained_models_folder, "generator_stable", f"epoch_{epoch:05d}")
+                os.makedirs(gen_path, exist_ok=True)
+                self.generator.save(gen_path)
+                    
+
+            wass_losses.append(float(wass_loss.numpy()))
+            disc_losses_f.append(float(disc_loss_f.numpy()))
+            disc_losses_r.append(float(disc_loss_r.numpy()))
+                
+            adv_losses.append(float(adv_loss.numpy()))
+            psd_losses.append(float(psd_loss.numpy()))
+            percents.append(float(percent.numpy()))
+                
+            grad_pen.append(float(gp.numpy()))
+            ratios1.append(float(ratio1.numpy()))
+            epoch_vect.append(epoch)
+
+            checkpoint.epoch.assign(epoch)
+            checkpoint_manager.save()
+
+
+            # Guardamos pérdidas en archivo
+            tmp_file = loss_file + ".tmp"
+            with open(tmp_file, 'w') as f:
+                json.dump({
+                        'epoch_vect' : epoch_vect,
+                        'disc_losses_f' : disc_losses_f,
+                        'disc_losses_r' : disc_losses_r,
+                        'wass_losses': wass_losses,
+                        'adv_losses': adv_losses,
+                        'grad_pen' : grad_pen,
+                        'psd_losses': psd_losses,
+                        'percents' : percents,
+                        'best_psd' : best_psd, 
+                        'best_epoch_psd' : best_epoch_psd,
+                        'best_percent' : best_percent,
+                        'best_epoch_percent' : best_epoch_percent,
+                        'ratio1' : ratios1, 
+                    }, f)
+            os.replace(tmp_file, loss_file)
+
+
+            plot_loss_graph(epoch_vect, wass_losses, adv_losses, "Wasserstein-Loss.pdf", "Wasserstein Loss", "Adv Loss", self.generated_images_folder)
+            plot_loss_graph(epoch_vect, disc_losses_f, disc_losses_r, "Distance-Loss.pdf", "Disc Loss Fake", "Disc Loss Real", self.generated_images_folder)
+            plot_loss_graph(epoch_vect, grad_pen, None,  "Gradient-penalty.pdf", "Gradient Penalty", "GP" ,self.generated_images_folder)
+                
