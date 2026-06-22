799a2267e7ca117bc49c1c839e5dd39dc9574509
María <maimu2@alumni.uv.es>
2026-06-22 12:50:40 +0200

    creación de modelos

--- Models3D/Modelo13.py
+++ Models3D/Modelo13.py
@@ -1,7 +1,7 @@
 """
-Datos4 + Arquitectura1
-Datos: forward(delta + 1) + Normalización +salida 'tanh' en el Generador
-Generator_film3
+Datos3 + Arquitectura1
+Datos: forward(delta+1)+salida 'Linear' en el Generador
+Generator_film_linear
 Discriminator_projection
 """
 import os
@@ -25,24 +25,25 @@ for gpu in gpus:
 
 
 from preprocess_data import Dataset
-from config import batch_size1, ncritic3, n_bar
-from architectures.generators import Generator_concat_linear
-from architectures.discriminators import Discriminator_concat
-from training2 import Training2
-from transforms import forward_1
+from config import batch_size2, ncritic3, n_bar
+from architectures.generators import Generator_film_linear_swish
+from architectures.discriminators import Discriminator_projection_swish
+from training import Training
+from transforms import forward_2
 from psd_utils import lambda_psd_schedule
 
 
-trained_models_folder = "Training3D/3-models"
-generated_images_folder = "Training3D/3-images"
+trained_models_folder = "Training3D/13-models"
+generated_images_folder = "Training3D/13-images"
 
 
 #Cargamos las clases necesarias
-datos= Dataset(batch_size1, n_bar, buffer_size = 918)
+datos= Dataset(batch_size2, n_bar, buffer_size = 918)
 
 #Cargamos los datos: número de partículas y redshift
 n_part, red = datos.load_npart("Data3D-64.hdf5")
-forw = forward_1(n_part)
+delta = datos.delta(n_part)
+forw = forward_2(delta+1)
 
 #Normalizamos el redshift
 z_vals = datos.factor_escala(red)
@@ -53,12 +54,12 @@ psd_max, psd_min, mean_psd, psd_sigma, _ = datos.load_psd("PSD_delta.npz")
 dataset = datos.crea_dataset(forw, z_vals, psd_max, psd_min, mean_psd, psd_sigma)
 
 #Cargamos el Discriminador y Generador
-generator = Generator_concat_linear(filter1 = 256, filter2 = 128, filter3 = 64)
-discriminator = Discriminator_concat(filter1 = 32, filter2 = 64, filter3 = 128, layer = "F")
+generator = Generator_film_linear_swish(filter1 = 256, filter2 = 128, filter3 = 64)
+discriminator = Discriminator_projection_swish(filter1 = 32, filter2 = 64, filter3 = 128, layer = "F")
 
 
 #Cargamos la red principal
-cgan = Training2(data_class = datos, discriminator = discriminator, generator = generator, batch_size = batch_size1, ncritic = ncritic3, 
+cgan = Training(data_class = datos, discriminator = discriminator, generator = generator, batch_size = batch_size2, ncritic = ncritic3, 
                 trained_models_folder = trained_models_folder, generated_images_folder = generated_images_folder, lambda_psd_schedule = lambda_psd_schedule,
                 lambda_term = 20, use_psd = False, use_psd_loss = False)
 cgan.compile(d_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.00005, beta_1 = 0, beta_2 = 0.9),
--- Models3D/Modelo8.py
+++ Models3D/Modelo8.py
@@ -1,8 +1,8 @@
 """
-Nuevo archivo en el que cambio los datos. Ahora los calculo como arcsinh(alpha*delta) y luego normalizo entre -1 y 1. Alpha = 1
-Uso GP de momento, sin SN
-Pongo LeakyReLU(0.2) en el Generador_film; además pongo use_bias  = True si no hay BN
-El discriminador es el de projection
+Datos3 + Arquitectura1
+Datos: forward(delta+1)+salida 'Linear' en el Generador
+Generator_film_linear
+Discriminator_projection
 """
 import os
 import tensorflow as tf
@@ -26,11 +26,11 @@ for gpu in gpus:
 
 from preprocess_data import Dataset
 from config import batch_size1, ncritic3, n_bar
-from architectures.generators import Generator_film3
+from architectures.generators import Generator_film_linear
 from architectures.discriminators import Discriminator_projection
-from training import Training
+from training8 import Training8
+from transforms import forward_2
 from psd_utils import lambda_psd_schedule
-
 
 
 trained_models_folder = "Training3D/8-models"
@@ -38,38 +38,28 @@ generated_images_folder = "Training3D/8-images"
 
 
 #Cargamos las clases necesarias
-datos= Dataset(batch_size1, n_bar, buffer_size = 918*4)
+datos= Dataset(batch_size1, n_bar, buffer_size = 918)
 
 #Cargamos los datos: número de partículas y redshift
 n_part, red = datos.load_npart("Data3D-64.hdf5")
-rot1 = datos.rotation(n_part, k=1)
-rot2 = datos.rotation(n_part, k=2)
-rot3 = datos.rotation(n_part, k=3)
-
-total_data = np.concatenate([n_part, rot1, rot2, rot3], axis = 0)
-#Aplicacmos la transformación al número de partículas. Esto es lo que recibirá la red. Ya está en el rango correcto
-n_part_transf = datos.transform_npart(total_data, k=11000)
+delta = datos.delta(n_part)
+forw = forward_2(delta+1)
 
 #Normalizamos el redshift
 z_vals = datos.factor_escala(red)
-z_vals = np.tile(z_vals, (4, 1))
 
 
-psd_max, psd_min, mean_psd, psd_sigma, _ = datos.load_psd("PSD_k11000.npz")
-psd_max = np.tile(psd_max, (4, 1))
-psd_min = np.tile(psd_min, (4, 1))
-mean_psd = np.tile(mean_psd, (4, 1))
-psd_sigma = np.tile(psd_sigma, (4, 1))
+psd_max, psd_min, mean_psd, psd_sigma, _ = datos.load_psd("PSD_delta.npz")
 
-dataset = datos.crea_dataset(n_part_transf, z_vals, psd_max, psd_min, mean_psd, psd_sigma)
+dataset = datos.crea_dataset(forw, z_vals, psd_max, psd_min, mean_psd, psd_sigma)
 
 #Cargamos el Discriminador y Generador
-generator = Generator_film3(filter1 = 256, filter2 = 128, filter3 = 64)
+generator = Generator_film_linear(filter1 = 256, filter2 = 128, filter3 = 64)
 discriminator = Discriminator_projection(filter1 = 32, filter2 = 64, filter3 = 128, layer = "F")
 
 
 #Cargamos la red principal
-cgan = Training(data_class = datos, discriminator = discriminator, generator = generator, batch_size = batch_size1, ncritic = ncritic3, 
+cgan = Training8(data_class = datos, discriminator = discriminator, generator = generator, batch_size = batch_size1, ncritic = ncritic3, 
                 trained_models_folder = trained_models_folder, generated_images_folder = generated_images_folder, lambda_psd_schedule = lambda_psd_schedule,
                 lambda_term = 20, use_psd = False, use_psd_loss = False)
 cgan.compile(d_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.00005, beta_1 = 0, beta_2 = 0.9),
